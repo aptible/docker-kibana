9cdd57d763bf1dbbeee9338d6a469403d0585e0a
Thomas Orozco <thomas@orozco.fr>
2016-11-25 13:51:45 +0100

    Support Kibana 5 / Elasticsearch 5

--- bin/run-kibana.sh
+++ bin/run-kibana.sh
@@ -29,14 +29,16 @@ if [[ -z "$KIBANA_ACTIVE_VERSION" ]]; then
   KIBANA_VERSION_PARSER="
   require 'json'
   version = JSON.parse(STDIN.read)['version']['number']
-  print version.start_with?('1.') ? 41 : 44"
+  print 41 if version.start_with?('1.')
+  print 44 if version.start_with?('2.')
+  print 5 if version.start_with?('5.')"
   KIBANA_ACTIVE_VERSION="$(curl "$DATABASE_URL" 2>/dev/null | ruby -e "$KIBANA_VERSION_PARSER" 2>/dev/null)"
 fi
 
 # If we still don't have a version, fall back to the default.
 if [[ -z "$KIBANA_ACTIVE_VERSION" ]]; then
-    echo "Warning: unable to fetch Elasticsearch version, and none configured. Defaulting to 4.4. Consider setting KIBANA_ACTIVE_VERSION."
-    KIBANA_ACTIVE_VERSION="44"
+    echo "Warning: unable to fetch Elasticsearch version, and none configured. Defaulting to 5.x. Consider setting KIBANA_ACTIVE_VERSION."
+    KIBANA_ACTIVE_VERSION="5"
 fi
 
 echo "KIBANA_ACTIVE_VERSION is set to: '$KIBANA_ACTIVE_VERSION'"
@@ -45,7 +47,7 @@ KIBANA_VERSION_PTR="KIBANA_${KIBANA_ACTIVE_VERSION}_VERSION"
 KIBANA_VERSION="${!KIBANA_VERSION_PTR}"
 
 # Run config
-erb -T 2 -r uri "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml.erb" > "/opt/kibana-${KIBANA_VERSION}-linux-x64/config/kibana.yml" || {
+erb -T 2 -r uri "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml.erb" > "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml" || {
   echo "Error creating kibana config file"
   exit 1
 }
@@ -58,4 +60,4 @@ service nginx start
 : ${NODE_OPTIONS:="--max-old-space-size=256"}
 
 export NODE_OPTIONS
-exec "/opt/kibana-${KIBANA_VERSION}-linux-x64/bin/kibana"
+exec "/opt/kibana-${KIBANA_VERSION}/bin/kibana"
