fc06779a70eba8b08e6f726c8145a5224614d7b6
Alex Kubacki <alex@aptible.com>
2019-05-07 12:49:36 -0400

    Add 6.7 support.

---
+++ 6.7/config.mk
@@ -0,0 +1,4 @@
+export KIBANA_VERSION=6.7.2
+export KIBANA_DOWNLOAD=https://artifacts.elastic.co/downloads/kibana/kibana-oss-$(KIBANA_VERSION)-linux-x86_64.tar.gz
+export KIBANA_SHA1SUM=ab374b2bbf131840296abada07c8ab940a5d6adc
+export OS_VERSION=14.04
